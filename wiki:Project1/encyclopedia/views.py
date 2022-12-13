7da1123a0dfc2b58871f7faa361d43cc06c2952b
Ronak Patel <ronaakk6@gmail.com>
2022-12-13 14:28:52 -0500

    New page functionality pretty much complete, need to fix redirect

--- wiki:Project1/encyclopedia/views.py
+++ wiki:Project1/encyclopedia/views.py
@@ -1,5 +1,6 @@
 import markdown
 from django import forms
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -30,13 +31,14 @@ def entry(request, title):
         })
 
 
+# Search a query
+
 # Making a search form
 class SearchForm(forms.Form):
     query = forms.CharField(widget=forms.TextInput(
         attrs={'placeholder': 'Search Encyclopedia'}), min_length=1, label="")
 
 
-# Search a query
 def search(request):
     if request.method == "POST":
         form = SearchForm(request.POST)
@@ -61,4 +63,56 @@ def search(request):
                 })
 
     # If page is not valid or not posted, redirect to main page
-    return HttpResponseRedirect(reverse("encyclopedia:index.html"))
+    return HttpResponseRedirect(reverse("encyclopedia:index"))
+
+
+# Creating a new Page
+
+# Making a new page form
+class NewPageForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={
+        "placeholder": "Page Title"}))
+    content = forms.CharField(label='', widget=forms.Textarea(attrs={
+        "placeholder": "Enter Page Content using Github Markdown"
+    }))
+
+
+def create(request):
+
+    # When the link in the navbar is clicked, load the form
+    if request.method == "GET":
+        return render(request, "encyclopedia/create.html", {
+            "create_form": NewPageForm(),
+            "search_form": SearchForm()
+        })
+
+    # When save button is hit
+    elif request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            new_entry = form.cleaned_data["title"]
+            new_content = form.cleaned_data["content"]
+            # If the title already exists
+            if util.get_entry(new_entry):
+                messages.error(request,
+                               "Oops! A encyclopedia with this name already exists. Choose a different name or edit the one that currently exists!")
+                return render(request, "encyclopedia/create.html", {
+                    "search_form": SearchForm(),
+                    "create_form": form
+                })
+            else:
+                # Save the entry otherwise
+                util.save_entry(new_entry, new_content)
+                messages.success(request,
+                                 f"New page: '{new_entry}' created successfully!")
+                return render(request, "encyclopedia/entry.html", {
+                    "title": new_entry.capitalize(),
+                    "search_form": SearchForm(),
+                    "entry": markdown.markdown(util.get_entry(new_entry))
+                })
+    else:
+        # In the case the user did not provide a valid title
+        messages.error(request, "Entry form not valid, please try again!")
+        return render(request, "encyclopedia/create.html", {
+            "create_form": form
+        })
---
+++ wiki:Project1/entries/Java.md
@@ -0,0 +1,11 @@
+# Java
+## What is Java technology and why do I need it?
+
+Java is a programming language and computing platform first released by Sun Microsystems in 1995. It has evolved from humble beginnings to power a large share of today’s digital world, by providing the reliable platform upon which many services and applications are built. New, innovative products and digital services designed for the future continue to rely on Java, as well.
+
+While most modern Java applications combine the Java runtime and application together, there are still many applications and even some websites that will not function unless you have a desktop Java installed. Java.com, this website, is intended for consumers who may still require Java for their desktop applications – specifically applications targeting Java 8. Developers as well as users that would like to learn Java programming should visit [oracle.com/java](https://www.oracle.com/java/) for more information.
+
+## Is Java free to download?
+
+Yes, Java is free to download for personal use.
+Java is also free for development: developers can find all the development kits and other useful tools at [https://www.oracle.com/javadownload/](https://www.oracle.com/javadownload/).
